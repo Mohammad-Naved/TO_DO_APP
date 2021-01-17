f6cc5216e052ae5a98a1ebc527cac39f37deef24
Mohammad-Naved <74970362+Mohammad-Naved@users.noreply.github.com>
2021-01-18 01:14:08 +0530

    initial-commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var add = $("#addItem");
+var sort = $("#sortItem");
+var reset = $("#resetItem");
+var clean = $("#cleanItem");
+var taskInput = $("#taskName");
+var listItems = $(".listItems");
+
+
+
+
+
+document.addEventListener("keypress",((event)=>{
+  if(event.key==='Enter')
+  {
+    clickitem();
+  }
+})
+)
+add.click(() => {
+ 
+  clickitem();
+});
+
+reset.click(()=>{
+  taskInput.val(" ");
+})
+
+clean.click(()=>{
+  $(".completed").remove();
+})
+sort.click(()=>{
+  $('ul .completed').appendTo(listItems)
+})
+
+
+
+  function clickitem(){
+    if (taskInput.val()!== '') {
+      var list = $("<li>", {
+        class: "list-group-item item ",
+        text: taskInput.val(),
+      });
+      listItems.append(list);
+      taskInput.val(" ");
+      list.click((event) => {
+        $(event.target).toggleClass("completed");
+      });
+    }
+    else  {
+      alert("There is no input");
+    }
+}
+
